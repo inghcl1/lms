6fb5c34fd950d27a18a472ada613fa60a689878b
User1 <User1@LP-5CD9296CML.HCLT.CORP.HCL.IN>
2019-09-12 13:08:24 +0530

    initial setup

--- src/main/java/com/hcl/lms/dto/ResponseDto.java
+++ src/main/java/com/hcl/lms/dto/ResponseDto.java
@@ -6,20 +6,25 @@ import lombok.Setter;
 @Setter
 @Getter
 public class ResponseDto {
-	
+
 	private String message;
+
+	private Integer statusCode;
+
 	public String getMessage() {
 		return message;
 	}
+
 	public void setMessage(String message) {
 		this.message = message;
 	}
+
 	public Integer getStatusCode() {
 		return statusCode;
 	}
+
 	public void setStatusCode(Integer statusCode) {
 		this.statusCode = statusCode;
 	}
-	private Integer statusCode;
 
 }
