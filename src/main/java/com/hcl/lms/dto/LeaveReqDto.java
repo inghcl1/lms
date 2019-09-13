6e7bc7f080bdb2467aef496c83c1811c5620cad9
User1 <User1@LP-5CD9296CTD.HCLT.CORP.HCL.IN>
2019-09-13 10:52:58 +0530

    sai controller test casess added

--- src/main/java/com/hcl/lms/dto/LeaveReqDto.java
+++ src/main/java/com/hcl/lms/dto/LeaveReqDto.java
@@ -5,58 +5,37 @@ import java.time.LocalDate;
 
 public class LeaveReqDto {
 	
-	public Integer getUserId() {
-		return userId;
-	}
-	public void setUserId(Integer userId) {
-		this.userId = userId;
-	}
-	public Integer getLeaveTypeId() {
-		return leaveTypeId;
-	}
-	public void setLeaveTypeId(Integer leaveTypeId) {
-		this.leaveTypeId = leaveTypeId;
-	}
-	public LocalDate getAppliedLeaveDate() {
-		return appliedLeaveDate;
-	}
-	public void setAppliedLeaveDate(LocalDate appliedLeaveDate) {
-		this.appliedLeaveDate = appliedLeaveDate;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	private Integer userId;
-	public Integer getUserId() {
-		return userId;
-	}
-	public void setUserId(Integer userId) {
-		this.userId = userId;
-	}
-	public Integer getLeaveTypeId() {
-		return leaveTypeId;
-	}
-	public void setLeaveTypeId(Integer leaveTypeId) {
-		this.leaveTypeId = leaveTypeId;
-	}
-	public LocalDate getAppliedLeaveDate() {
-		return appliedLeaveDate;
-	}
-	public void setAppliedLeaveDate(LocalDate appliedLeaveDate) {
-		this.appliedLeaveDate = appliedLeaveDate;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
 	private Integer leaveTypeId;
 	private LocalDate appliedLeaveDate;
 	private String description;
+	private Integer userId;
+	public Integer getLeaveTypeId() {
+		return leaveTypeId;
+	}
+	public void setLeaveTypeId(Integer leaveTypeId) {
+		this.leaveTypeId = leaveTypeId;
+	}
+	public LocalDate getAppliedLeaveDate() {
+		return appliedLeaveDate;
+	}
+	public void setAppliedLeaveDate(LocalDate appliedLeaveDate) {
+		this.appliedLeaveDate = appliedLeaveDate;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	
+	
 	
 	}
 	
--- src/main/java/com/hcl/lms/dto/LeaveResDto.java
+++ src/main/java/com/hcl/lms/dto/LeaveResDto.java
@@ -2,32 +2,22 @@ package com.hcl.lms.dto;
 
 public class LeaveResDto {
 
-	public String getMessage() {
-		return message;
-	}
-	public void setMessage(String message) {
-		this.message = message;
-	}
-	public Integer getStatusCode() {
-		return statusCode;
-	}
-	public void setStatusCode(Integer statusCode) {
-		this.statusCode = statusCode;
-	}
-	private String message;
-	public String getMessage() {
-		return message;
-	}
-	public void setMessage(String message) {
-		this.message = message;
-	}
-	public Integer getStatusCode() {
-		return statusCode;
-	}
-	public void setStatusCode(Integer statusCode) {
-		this.statusCode = statusCode;
-	}
 	private Integer statusCode;
+	private String message;
+
+	
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public Integer getStatusCode() {
+		return statusCode;
+	}
+	public void setStatusCode(Integer statusCode) {
+		this.statusCode = statusCode;
+	}
 	
 	
 
